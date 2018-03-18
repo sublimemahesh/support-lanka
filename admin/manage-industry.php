cbbf98c121e3a30d1c1a34cef2873e45b35f91b5
mayomi <mayomi@sublime.lk>
2018-03-18 14:53:56 +0530

    #5 Create Manage Industries in Admin panel

---
+++ admin/manage-industry.php
@@ -0,0 +1,136 @@
+﻿<?php
+include_once(dirname(__FILE__) . '/../class/include.php');
+include_once(dirname(__FILE__) . '/auth.php');
+
+$INDUSTRY = new Industry(NULL)
+?> 
+﻿<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <title>Manage Industry - www.srilankatourism.travel</title>
+        <!-- Favicon-->
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+        <!-- Bootstrap Core Css -->
+        <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+        <!-- Waves Effect Css -->
+        <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+
+        <!-- Animation Css -->
+        <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+
+        <!-- JQuery DataTable Css -->
+        <link href="plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
+        <link href="plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+        <!-- Custom Css -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <link href="css/themes/all-themes.css" rel="stylesheet" />
+    </head>
+
+    <body class="theme-red">
+        <?php
+        include './navigation-and-header.php';
+        ?>
+        <section class="content">
+            <div class="container-fluid"> 
+                <!-- Manage Industry -->
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <div class="header">
+                                <h2>
+                                    Manage Industry
+                                </h2>
+                                <ul class="header-dropdown">
+                                    <li>
+                                        <a href="create-industry.php">
+                                            <i class="material-icons">add</i> 
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="body">
+                                <div>
+                                    <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Name</th> 
+                                                <th>Options</th>
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Name</th> 
+                                                <th>Options</th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <?php
+                                            foreach ($INDUSTRY->all() as $key => $indu) {
+                                                ?>
+                                                <tr id="row_<?php echo $indu['id']; ?>">
+                                                    <td><?php echo $indu['sort']; ?></td> 
+                                                    <td><?php echo $indu['name']; ?></td> 
+                                                    <td> 
+                                                        <a href="edit-industry.php?id=<?php echo $indu['id']; ?>" class="op-link"><i class="glyphicon glyphicon-pencil"></i></a>  |  
+
+                                                        <a href="#" class="delete-industry" data-id="<?php echo $indu['id']; ?>">
+                                                            <button class="glyphicon glyphicon-trash" data-type="cancel"></button>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                                <?php
+                                            }
+                                            ?>   
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <script src="plugins/jquery/jquery.min.js"></script>
+
+        <script src="plugins/bootstrap/js/bootstrap.js"></script>
+
+        <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+        <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+        <script src="plugins/node-waves/waves.js"></script>
+
+        <script src="plugins/jquery-datatable/jquery.dataTables.js"></script>
+        <script src="plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+        <script src="plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+        <script src="plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+        <script src="plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+        <script src="plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+        <script src="plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+        <script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+        <script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+        <script src="plugins/sweetalert/sweetalert.min.js"></script>
+        <!-- Custom Js -->
+        <script src="js/admin.js"></script>
+        <script src="js/pages/tables/jquery-datatable.js"></script>
+
+        <!-- Demo Js -->
+        <script src="js/demo.js"></script>
+        <script src="delete/js/industry.js" type="text/javascript"></script>
+    </body>
+
+</html> 
