d25bc0b703ba944f7afa60615f2ea794b7d06755
mayomi <mayomi@sublime.lk>
2018-03-18 12:17:09 +0530

    #3 Admin Class City, District

---
+++ admin/create-city.php
@@ -0,0 +1,120 @@
+<?php
+include_once(dirname(__FILE__) . '/../class/include.php');
+include_once(dirname(__FILE__) . './auth.php');
+?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <title>Add New City - www.srilankatourism.travel</title>
+        <!-- Favicon-->
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+        <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+        <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+        <link href="plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/themes/all-themes.css" rel="stylesheet" />
+        <!-- Bootstrap Spinner Css -->
+        <link href="plugins/jquery-spinner/css/bootstrap-spinner.css" rel="stylesheet">
+    </head>
+
+    <body class="theme-red">
+        <?php
+        include 'navigation-and-header.php';
+        ?>
+
+        <section class="content">
+            <div class="container-fluid">  
+                <?php
+                $vali = new Validator();
+
+                $vali->show_message();
+                ?>
+                <!-- Vertical Layout -->
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <div class="header">
+                                <h2>Create City</h2>
+                                <ul class="header-dropdown">
+                                    <li class="">
+                                        <a href="manage-city.php">
+                                            <i class="material-icons">list</i> 
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="body">
+                                <form class="form-horizontal"  method="post" action="post-and-get/city.php" enctype="multipart/form-data"> 
+                                    <div class="row clearfix">
+                                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                            <label for="district">District</label>
+                                        </div>
+                                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                            <div class="form-group place-select">
+                                                <div class="form-line">
+                                                    <select class="form-control place-select1 show-tick" autocomplete="off" type="text" id="type" autocomplete="off" name="district" required="TRUE">
+                                                        <option value=""> -- Please Select -- </option>
+                                                        <?php foreach (District::all() as $key => $district) {
+                                                            ?>
+                                                            <option value="<?php echo $district['id']; ?>"><?php echo $district['name']; ?></option><?php
+                                                        }
+                                                        ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="row clearfix">
+                                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                            <label for="name">Name</label>
+                                        </div>
+                                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                            <div class="form-group">
+                                                <div class="form-line">
+                                                    <input type="text" id="name" class="form-control" placeholder="Enter Product name" autocomplete="off" name="name" >
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                            </div>
+
+                            <div class="row clearfix">
+                                <div class="col-lg-offset-2 col-md-offset-2 col-sm-offset-4 col-xs-offset-5"> 
+                                    <input type="submit" name="add-city" class="btn btn-primary m-t-15 waves-effect" value="Add City"/>
+                                </div>
+                            </div>
+
+
+                            <hr/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Vertical Layout -->
+
+        </div>
+    </section>
+
+    <!-- Jquery Core Js -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.js"></script> 
+    <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <script src="plugins/node-waves/waves.js"></script>
+    <script src="plugins/jquery-spinner/js/jquery.spinner.js"></script>
+    <script src="js/admin.js"></script>
+    <script src="js/demo.js"></script>
+
+
+</body>
+
+</html>
---
+++ admin/post-and-get/district.php
@@ -0,0 +1,71 @@
+<?php
+
+include_once(dirname(__FILE__) . '/../../class/include.php');
+
+include_once(dirname(__FILE__) . '/../auth.php');
+
+
+if (isset($_POST['add-district'])) {
+    $DISTRICT = new District(NULL);
+    $VALID = new Validator();
+
+    $DISTRICT->name = filter_input(INPUT_POST, 'name');
+
+    $VALID->check($DISTRICT, ['name' =>
+            ['required' => TRUE]
+    ]);
+
+    if ($VALID->passed()) {
+        $DISTRICT->create();
+
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+        $VALID->addError("Your data was saved successfully", 'success');
+        $_SESSION['ERRORS'] = $VALID->errors();
+
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    } else {
+
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        $_SESSION['ERRORS'] = $VALID->errors();
+
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }
+}
+
+if (isset($_POST['edit-district'])) {
+    $DISTRICT = new District(NULL);
+
+    $DISTRICT->id = $_POST['id'];
+    $DISTRICT->name = $_POST['name'];
+
+    $VALID = new Validator();
+    $VALID->check($DISTRICT, ['name' =>
+            ['required' => TRUE]
+    ]);
+
+    if ($VALID->passed()) {
+        $DISTRICT->update();
+
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+        $VALID->addError("Your changes saved successfully", 'success');
+        $_SESSION['ERRORS'] = $VALID->errors();
+
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    } else {
+
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        $_SESSION['ERRORS'] = $VALID->errors();
+
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }
+}
