ceb4ca517d9945ca36f240fc8c1c807494deefec
chalanadulaj <chalanadulaj99@gmail.com>
2018-05-08 12:14:48 +0530

    #144 Change index design

--- member/header-nav.php
+++ member/header-nav.php
@@ -9,7 +9,7 @@ $MEMBER = new Member($_SESSION['id']);
         <div class="fa fa-bars"></div>
     </div>
     <!--logo start-->
-    <a href="index.php" class="logo " id="new-font"> <b>Member Dashboard</b></a>
+    <a href="index-profile.php" class="logo " id="new-font"> <b> Dashboard</b></a>
     <!--logo end-->
     <div class="pull-right top-menu nav notify-row new-notify-row">
         <ul class="nav top-menu">
