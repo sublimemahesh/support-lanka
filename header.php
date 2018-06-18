920a6103d3f35cf9882d02436b332e6bbd3f3ab7
chalanadulaj <chalanadulaj99@gmail.com>
2018-06-18 13:59:36 +0530

    #225 re correction header

--- header.php
+++ header.php
@@ -2,42 +2,43 @@
 
 <div class="responsive-header">
     <div class="row">
-
-        <div class="col-md-12">
-
+        <div class="res-logo col-md-9">
             <a href="index.php">
-                <div class="res-logo col-md-9">
-                    <img src="images/resource/logo11.png" alt=""  class="img-new"/>
-                    <div class="pull-right translation-links" style="margin-right: -13px;margin-top: -10px; margin-left:26px "> 
-                        <a href=""  class="Sinhala notranslate" data-lang="Sinhala" > <img src="images/Word_Sinhala_in_Yasarath_font.svg.png" alt="" width="60px" class="pull-leftss"/> </a>
-                        <img src="images/line.png" alt="" width="10px"/>
-                        <a href=""  class="English notranslate" data-lang="English" ><img src="images/english.png" alt="" width="60px"/></a>
-                    </div>
+                <img src="images/resource/logo11.png" alt=""  class="img-new"/>
+            </a>
+            <div class=" pull-left translation-links new-style">
+
+                <a href=""  class="Sinhala notranslate" data-lang="Sinhala" > 
+                    <img src="images/Word_Sinhala_in_Yasarath_font.svg.png" alt="" width="60px" class="pull-leftss"/> </a>
+                <img src="images/line.png" alt="" width="10px"/>
+
+                <a href=""  class="English notranslate" data-lang="English" >
+                    <img src="images/english.png" alt="" width="60px"/>
+                </a>
+            </div>
+        </div> 
+
+        <div class="menu-resaction col-md-3">
+            <div class="res-openmenu">
+                <i class="la la-list nav-icon"></i>
+            </div>
+            <a href="index.php"> <div class="res-closemenu">
+                    <img src="images/icon2.png" alt="" />
                 </div>
             </a>
-
-            <div class="menu-resaction col-md-3">
-
-                <div class="res-openmenu">
-                    <i class="la la-list nav-icon"></i>
-                </div>
-                <a href="index.php"> <div class="res-closemenu">
-                        <img src="images/icon2.png" alt="" />
-                    </div>
-                </a>
-
-            </div>
-            <div class="new-btn-p2">
-
-                <a href="member/login.php" title="" id="post-job-btn1"class=" sticky1 btn3" ><i class="la la-user"></i>Login Or Register</a>
-            </div>
         </div>
     </div>
+    <div class="row pull-right p-bottom-h" >
+        <div class="new-btn-p2">
+            <a href="member/login.php" title="" id="post-job-btn1"class=" sticky1 btn3" ><i class="la la-user"></i>Login Or Register</a>
+        </div>
+    </div>
+
+
+
     <div class="responsive-opensec">
         <div class="btn-extars ">
-
             <a href="company/register.php" title="" id="post-job-btn1"class=" sticky1 btn2"><i class="la la-plus"></i>Post Vacancy</a>
-
         </div><!-- Btn Extras -->
 
         <div class="responsivemenu">
@@ -69,32 +70,24 @@
 </div>                                               
 
 <header class="stick-top forsticky">
-
     <div class="menu-sec new-height">
-        <div class="pull-right translation-links" style="margin-right: 110px;margin-top: -45px;"> 
+        <div class="pull-right translation-links p-pad"> 
             <a href=""  class="Sinhala notranslate" data-lang="Sinhala" > <img src="images/Word_Sinhala_in_Yasarath_font.svg.png" alt="" width="60px" class="pull-leftss"/> </a>
             <img src="images/line.png" alt="" width="10px"/>
             <a href=""  class="English notranslate" data-lang="English" ><img src="images/english.png" alt="" width="60px"/></a>
         </div>
         <div class="container new-height " >
-
             <div class="logo">
-
                 <a href="index.php" title=""><img class="hidesticky" src="images/resource/logo11.png" alt="" /><img class="showsticky" src="images/resource/logo10.png" alt="" /></a>
             </div> 
             <div class="btn-extars"> 
-
                 <ul class="account-btn">
-
                     <a href="member/login.php" title="" id="post-job-btn1"class=" sticky1 btn2" style="padding-right: 20px;"><i class="la la-user"></i>Login Or Register</a>
                 </ul>
                 <a href="company/register.php" title="" id="post-job-btn1"class=" sticky1 btn2"><i class="la la-plus"></i>Post Vacancy</a>
 
             </div><!-- Btn Extras -->
-            <div>
-
-
-            </div>
+           
             <nav>
                 <ul>
                     <li class="">
